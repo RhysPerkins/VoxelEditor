efdedd87a7b30d5a2fd1e98d1836443ce6ddc2bf
Rhys Perkins <rhysperkins@outlook.com>
2014-11-06 11:32:37 +0000

    Add camera classes and update vertex format
    Add camera classes
    Vertex format folder name change
    Vertex format namespace change

---
+++ src/VoxelEditor/Cameras/Camera.cs
@@ -0,0 +1,262 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Xml.Serialization;
+
+namespace VoxelMeshEditor.Cameras
+{
+    // Matrix Order
+    // ============
+    //
+    // Row Major
+    // ---------
+    // - XNA Default -> matrixView.Right -> M11 M12 M13
+    //   xAxis.X = matrix_View.M11;    
+    //   xAxis.Y = matrix_View.M12;    
+    //   xAxis.Z = matrix_View.M13;
+    //
+    // Column Major
+    // ------------
+    // - NOT XNA Default -> M11 M21 M31
+    //   xAxis.X = matrix_View.M11;    
+    //   xAxis.Y = matrix_View.M21;    
+    //   xAxis.Z = matrix_View.M31;
+    //
+    // Pixel Aspect Ratio
+    // ==================
+    // - Tells us the ratio of a pixel's height to its width
+    // 
+    //   xPhysicalPixelSize   xPhysicalHeightMonitor   xMonitorResolution
+    //   ------------------ = ---------------------- • ------------------
+    //   yPhysicalPixelSize   yPhysicalHeightMonitor   yMonitorResolution
+    //
+    // Projection Matrix
+    // =================
+    // - The projection matrix maps the camera-space coordinates into the screen space 
+    //   where "left of the screen" is -1, and "right of the screen" is 1, and same for top and bottom 
+    // - Using an orthographic projection removes all perspective from the camera's view 
+    //   This is mostly useful for making isometric or 2D games
+    // - For an orthographic width, you want the X coordinate to be -1 at "0" and 1 at "width" 
+    //   This means that the total length of the (x,y,z) vector in the first column should be 2 / width 
+    // - For example, if you want 500 to be the center of X, then you should make the last element -500 
+    //   (because 500 - 500 == 0, which is the center of the projected screen)
+
+    public enum MatrixOrder : byte
+    {
+        ColumnMajor,
+        RowMajor
+    }
+
+    [XmlInclude(typeof(FreeLookCameraSettings))]
+    public abstract class CameraSettings { }
+
+    /// <summary>
+    /// Abstract base class for all camera types
+    /// </summary>
+    public abstract class Camera
+    {
+        protected CollisionDetection.ThreeDimensional.BoundingFrustum boundingFrustum = new CollisionDetection.ThreeDimensional.BoundingFrustum();   // Bounding frustum                                                                               // Entity to be used for camera collision detection
+        protected float farClipPlaneZ;
+        protected float fov;
+        protected VoxelMeshEditorMain game;
+        protected bool isAlive = false;
+        protected bool isIgnoreInput = false;                                                                       // Ignores all input from HID devices
+        protected bool isLookInverted = false;                                                                      // Invert look for y-axis
+        protected float lookSensitivity;                                                                            // HID (Mouse/Gamepad) look sensitivity
+        protected MatrixOrder matrixOrder = MatrixOrder.RowMajor;                                                   // Set matrix order to XNA default
+        protected Matrix matrix_Projection;                                                                         // Projection Matrix
+        protected Matrix matrix_View;                                                                               // View Matrix
+        protected Matrix matrix_ViewProj;
+        protected float nearClipPlaneZ;
+        protected Quaternion orientation;                                                                           // Orientation/rotation 
+        protected float pitchAccum;                                                                                 // Accumulated degrees for pitch (Looking "Up" and "Down") 
+        protected Vector3 position;                                                                                 // Current position      
+        protected float speed;
+        protected Vector3 velocity;
+        protected float yawAccum;                                                                                   // Accumulated degrees for yaw (Looking "Left" and "Right")  
+
+        protected abstract void HandleInput();                                                     // Handles all HID input
+        public abstract void LookAt(Vector3 camPosition, Vector3 camTarget);                                        // Tells the camera to immediately look at a target from a position
+        public abstract void LookAt(Vector3 camPosition, Vector3 camTarget, float interpTime, GameTime gameTime);   // Tells the camera to interpolate towards a target from a position
+        public abstract void Move(Vector3 direction);
+        public abstract void Read(CameraSettings settings);
+        public abstract CameraSettings Write();
+        public abstract void Spawn(CameraSettings settings);
+        public abstract void Update(GameTime gameTime);                                                             // Updates camera (view matrix, etc.)
+        protected abstract void UpdatePerspective();                                                                // Updates the projection matrix
+        protected abstract void UpdateViewMatrix();                                                                 // Updates the view matrix
+        public abstract void Viewport(Viewport viewport);
+
+        public CollisionDetection.ThreeDimensional.BoundingFrustum BoundingFrustum
+        {
+            get { return boundingFrustum; }
+        }
+
+        public float FarClip
+        {
+            get { return farClipPlaneZ; }
+            set
+            {
+                farClipPlaneZ = value;
+                UpdatePerspective();
+            }
+        }
+
+        public Vector3 Forward
+        {
+            get
+            {
+                Vector3 forward = new Vector3();
+
+                switch (matrixOrder)
+                {
+                    case MatrixOrder.ColumnMajor:
+                        {
+                            forward.X = -matrix_View.M13;      // -ve due to -Z going into the screen
+                            forward.Y = -matrix_View.M23;
+                            forward.Z = -matrix_View.M33;
+                            break;
+                        }
+                    case MatrixOrder.RowMajor:
+                        {
+                            forward.X = -matrix_View.M31;
+                            forward.Y = -matrix_View.M32;
+                            forward.Z = -matrix_View.M33;
+                            break;
+                        }
+                }
+
+                return forward;
+            }
+        }
+
+        /// <summary>
+        /// Camera's field of view in radians (x axis)
+        /// </summary>
+        public float FOV
+        {
+            get { return fov; }
+            set
+            {
+                fov = value;
+                UpdatePerspective();
+            }
+        }
+
+        public bool IsIgnoreInput
+        {
+            get { return isIgnoreInput; }
+            set { isIgnoreInput = value; }
+        }
+
+        public bool IsLookInverted
+        {
+            get { return isLookInverted; }
+            set { isLookInverted = value; }
+        }
+
+        public float LookSensitivity
+        {
+            get { return lookSensitivity; }
+            set { lookSensitivity = value; }
+        }
+
+        public float NearClip
+        {
+            get { return nearClipPlaneZ; }
+            set
+            {
+                nearClipPlaneZ = value;
+                UpdatePerspective();
+            }
+        }
+
+        public Quaternion Orientation
+        {
+            get { return orientation; }
+            set
+            {
+                orientation = value;
+
+                // TODO: Handle IsInterpolating
+                UpdateViewMatrix();
+            }
+        }
+
+        public Vector3 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Matrix ProjectionMatrix
+        {
+            get { return matrix_Projection; }
+        }
+
+        public Vector3 Right
+        {
+            get
+            {
+                Vector3 right = new Vector3();
+
+                switch (matrixOrder)
+                {
+                    case MatrixOrder.ColumnMajor:
+                        {
+                            right.X = matrix_View.M11;
+                            right.Y = matrix_View.M21;
+                            right.Z = matrix_View.M31;
+                            break;
+                        }
+                    case MatrixOrder.RowMajor:
+                        {
+                            right.X = matrix_View.M11;
+                            right.Y = matrix_View.M12;
+                            right.Z = matrix_View.M13;
+                            break;
+                        }
+                }
+
+                return right;
+            }
+        }
+
+        public Vector3 Up
+        {
+            get
+            {
+                Vector3 up = new Vector3();
+
+                switch (matrixOrder)
+                {
+                    case MatrixOrder.ColumnMajor:
+                        {
+                            up.X = matrix_View.M12;
+                            up.Y = matrix_View.M22;
+                            up.Z = matrix_View.M32;
+                            break;
+                        }
+                    case MatrixOrder.RowMajor:
+                        {
+                            up.X = matrix_View.M21;
+                            up.Y = matrix_View.M22;
+                            up.Z = matrix_View.M23;
+                            break;
+                        }
+                }
+
+                return up;
+            }
+        }
+
+        public Matrix ViewMatrix
+        {
+            get { return matrix_View; }
+        }
+
+        public Matrix ViewProjectionMatrix
+        {
+            get { return matrix_ViewProj; }
+        }
+    }
+}
---
+++ src/VoxelEditor/Cameras/FreeLookCamera.cs
@@ -0,0 +1,352 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using VoxelMeshEditor.Mathematics;
+
+namespace VoxelMeshEditor.Cameras
+{
+    public class FreeLookCameraSettings : CameraSettings
+    {
+        public float FarClip = 1100.0f;
+        public float FOV = ToolBox.PIOVER4;       // 45 degrees
+        public bool IsIgnoreInput = false;
+        public float NearClip = 0.1f;
+        public Quaternion Orientation = Quaternion.Identity;
+        public Vector3 Position = Vector3.Zero;
+        public float Sensitivity = 0.3f;
+        public float Speed = 10f;
+    }
+
+    public class FreeLookCamera : Camera
+    {
+        float interpolationStart;           // Interpolation start time
+        float interpolationTime;            // Time period to perform interpolation over
+        bool isInterpolating = false;       // Is the camera interpolating between orientations?     
+        Quaternion orientationEnd;          // End orientation for interpolation
+        Quaternion orientationStart;        // Starting orientation for interpolation   
+        float yaw;                          // Current camera yaw                      
+
+        public FreeLookCamera(VoxelMeshEditorMain game)
+            : this(game, null) { }
+
+        public FreeLookCamera(VoxelMeshEditorMain game, FreeLookCameraSettings settings)
+        {
+            this.game = game;
+            matrixOrder = MatrixOrder.ColumnMajor;
+
+            Read(settings);
+        }
+
+        protected override void HandleInput()
+        {
+            if (isIgnoreInput) //|| !game.IsActiveApplication)
+                return;
+
+            HandleKeyboard();
+            HandleMouse();
+        }
+
+        private void HandleKeyboard()
+        {
+            Vector3 direction = Vector3.Zero;
+
+            if (game.HID.IsKeyDown(PlayerIndex.One, Keys.A))
+                direction += -Right;
+
+            if (game.HID.IsKeyDown(PlayerIndex.One, Keys.D))
+                direction += Right;
+
+            if (game.HID.IsKeyDown(PlayerIndex.One, Keys.S))
+                direction += -Forward;
+
+            if (game.HID.IsKeyDown(PlayerIndex.One, Keys.W))
+                direction += Forward;
+
+            Move(direction);
+        }
+
+        private void HandleMouse()
+        {
+            RotateSmoothly(game.HID.MouseMovement.X, game.HID.MouseMovement.Y);
+        }
+
+        public override void LookAt(Vector3 camPosition, Vector3 camTarget)
+        {
+            LookAt(camPosition, camTarget, 0, null);
+        }
+
+        public override void LookAt(Vector3 camPosition, Vector3 camTarget, float interpTime, GameTime gameTime)
+        {
+            if (isInterpolating)
+                return;
+
+            // Set the camera position to the new position
+            position = camPosition;
+
+            // zAxis
+            Vector3 v = position - camTarget;
+
+            // Euler angles from vector v (spherical angles)
+            //float l = (float)Math.Sqrt(v.X * v.X + v.Z * v.Z); 
+            float l = v.X * v.X + v.Z * v.Z;
+
+            if (l > ToolBox.EPSILON * ToolBox.EPSILON)
+            {
+                // Calculate current yaw based on camera forward (zAxis)
+                yaw = (float)Math.Atan2(v.X, v.Z);
+            }
+
+            float pitch = (float)-Math.Atan2(v.Y, l);
+
+            Matrix view = Matrix.CreateFromYawPitchRoll(yaw, pitch, 0);
+            view.Translation = position;
+            view = Matrix.Invert(view);
+
+            if (interpTime <= ToolBox.EPSILON)
+            {
+                matrix_View = view;
+
+                // Accumulated pitch and yaw degrees need to be updated with current look at Matrix values 
+                // (E.G. Look at target might be elevated by 80 degrees or 20 degrees to the left)
+                pitchAccum = MathHelper.ToDegrees(-pitch);
+                yawAccum = MathHelper.ToDegrees(yaw);
+
+                Quaternion.CreateFromRotationMatrix(ref matrix_View, out orientation);
+
+                // Create the combined view-projection matrix
+                Matrix.Multiply(ref matrix_View, ref matrix_Projection, out matrix_ViewProj);
+
+                // Update the bounding frustum
+                boundingFrustum.SetMatrix(matrix_ViewProj);
+            }
+            else
+            {
+                orientationStart = orientation;
+                Quaternion.CreateFromRotationMatrix(ref view, out orientationEnd);
+                interpolationStart = (float)gameTime.TotalGameTime.TotalSeconds;
+                interpolationTime = interpTime;
+                isInterpolating = true;
+            }
+        }
+
+        public override void Move(Vector3 direction)
+        {
+            float dtSeconds = (float)game.GameTime.ElapsedGameTime.TotalSeconds;
+
+            velocity = speed * direction;
+            position += velocity * dtSeconds;
+        }
+
+        /// <summary>
+        /// Set up Camera's Projection Matrix
+        /// </summary>
+        /// <param name="aspect_Ratio">Screen Resolution Aspect Ratio</param>
+        /// <param name="z_NearPlane">Near Clipping Plane</param>
+        /// <param name="z_FarPlane">Far Clipping Plane</param>
+        private void Perspective(float aspect_Ratio, float z_NearClipPlane, float z_FarClipPlane)
+        {
+            nearClipPlaneZ = z_NearClipPlane;
+            farClipPlaneZ = z_FarClipPlane;
+
+            float yZoom = 1f / (float)Math.Tan(fov * 0.5f);
+            float xZoom = yZoom / aspect_Ratio;
+
+            matrix_Projection.M11 = xZoom;
+            matrix_Projection.M12 = 0f;
+            matrix_Projection.M13 = 0f;
+            matrix_Projection.M14 = 0f;
+
+            matrix_Projection.M21 = 0f;
+            matrix_Projection.M22 = yZoom;
+            matrix_Projection.M23 = 0f;
+            matrix_Projection.M24 = 0f;
+
+            matrix_Projection.M31 = 0f;
+            matrix_Projection.M32 = 0f;
+            matrix_Projection.M33 = z_FarClipPlane / (nearClipPlaneZ - farClipPlaneZ);
+            matrix_Projection.M34 = -1f;
+
+            matrix_Projection.M41 = 0f;
+            matrix_Projection.M42 = 0f;
+            matrix_Projection.M43 = (nearClipPlaneZ * farClipPlaneZ) / (nearClipPlaneZ - farClipPlaneZ);
+            matrix_Projection.M44 = 0f;
+        }
+
+        private void Rotate(float yawDegrees, float pitchDegrees)
+        {
+            yawDegrees = -yawDegrees;
+
+            if (!isLookInverted)
+                pitchDegrees = -pitchDegrees;
+
+            pitchAccum += pitchDegrees;
+
+            // Stops the Player/Camera from looking any higher than 90 degrees [Mouse Y Movement]
+            if (pitchAccum > 90.0f)
+            {
+                pitchDegrees = 90.0f - (pitchAccum - pitchDegrees);
+                pitchAccum = 90.0f;
+            }
+
+            // Stops the Player/Camera from looking any lower than 90 degrees [Mouse Y Movement]
+            if (pitchAccum < -90.0f)
+            {
+                pitchDegrees = -90.0f - (pitchAccum - pitchDegrees);
+                pitchAccum = -90.0f;
+            }
+
+            yawAccum += yawDegrees;
+
+            if (yawAccum > 360.0f)
+                yawAccum -= 360.0f;
+
+            if (yawAccum < -360.0f)
+                yawAccum += 360.0f;
+
+            float yaw = MathHelper.ToRadians(yawAccum);
+            float pitch = MathHelper.ToRadians(pitchAccum);
+
+            orientation =
+                Quaternion.CreateFromAxisAngle(Vector3.UnitX, pitch) *
+                Quaternion.CreateFromAxisAngle(Vector3.UnitY, yaw);
+        }
+
+        /// <summary>
+        /// Dampens the rotation by applying the rotation speed to it
+        /// </summary>
+        /// <param name="yawDegrees">Y axis rotation in degrees. [Mouse X Movement]</param>
+        /// <param name="pitchDegrees">X axis rotation in degrees. [Mouse Y Movement]</param>
+        public void RotateSmoothly(float yawDegrees, float pitchDegrees)
+        {
+            yawDegrees *= lookSensitivity;
+            pitchDegrees *= lookSensitivity;
+
+            Rotate(yawDegrees, pitchDegrees);
+        }
+
+        public override CameraSettings Write()
+        {
+            FreeLookCameraSettings settings = new FreeLookCameraSettings();
+
+            // Assign current camera settings
+            settings.FarClip = farClipPlaneZ;
+            settings.IsIgnoreInput = isIgnoreInput;
+            settings.NearClip = nearClipPlaneZ;
+            settings.Orientation = orientation;
+            settings.Position = position;
+            settings.Sensitivity = lookSensitivity;
+            settings.Speed = speed;
+
+            return settings;
+        }
+
+        public override void Read(CameraSettings _settings)
+        {
+            FreeLookCameraSettings settings = _settings as FreeLookCameraSettings;
+
+            if (settings == null)
+                settings = new FreeLookCameraSettings();
+
+            farClipPlaneZ = settings.FarClip;
+            fov = settings.FOV;
+            isIgnoreInput = settings.IsIgnoreInput;
+            lookSensitivity = settings.Sensitivity;
+            nearClipPlaneZ = settings.NearClip;
+            orientation = settings.Orientation;
+            position = settings.Position;
+            speed = settings.Speed;
+
+            // Create Projection Matrix
+            Perspective(game.GraphicsDevice.Viewport.AspectRatio, nearClipPlaneZ, farClipPlaneZ);
+
+            // Update View Matrix
+            UpdateViewMatrix();
+
+            // Accumulated yaw and pitch will need updating because orientation and view matrix have changed
+            pitchAccum = MathHelper.ToDegrees((float)Math.Asin(matrix_View.M23));
+            yawAccum = MathHelper.ToDegrees((float)Math.Atan2(matrix_View.M13, matrix_View.M33));
+        }
+
+        public override void Spawn(CameraSettings _settings)
+        {
+            Read(_settings);
+
+            // Create Projection Matrix
+            Perspective(game.GraphicsDevice.Viewport.AspectRatio, nearClipPlaneZ, farClipPlaneZ);
+
+            // Update View Matrix
+            UpdateViewMatrix();
+
+            // Accumulated yaw and pitch will need updating because orientation and view matrix have changed
+            pitchAccum = MathHelper.ToDegrees((float)Math.Asin(matrix_View.M23));
+            yawAccum = MathHelper.ToDegrees((float)Math.Atan2(matrix_View.M13, matrix_View.M33));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Input is only handled when the camera is not interpolating
+            if (!isInterpolating)
+            {
+                HandleInput();
+            }
+            else
+            {
+                float dt = (float)gameTime.TotalGameTime.TotalSeconds - interpolationStart;
+
+                // Scale dt to the range of [0, 1] for slerping
+                float delta = dt / interpolationTime;
+
+                if (delta < 1f)
+                {
+                    orientation = Quaternion.Slerp(orientationStart, orientationEnd, delta);
+                }
+                else
+                {
+                    // Update accumulated pitch and yaw once interpolation has ended
+                    pitchAccum = MathHelper.ToDegrees((float)Math.Asin(matrix_View.M23));
+                    yawAccum = MathHelper.ToDegrees((float)Math.Atan2(matrix_View.M13, matrix_View.M33));
+
+                    // Flag interpolating as false
+                    isInterpolating = false;
+                }
+            }
+
+            UpdateViewMatrix();
+        }
+
+        protected override void UpdatePerspective()
+        {
+            Perspective(game.GraphicsDevice.Viewport.AspectRatio, nearClipPlaneZ, farClipPlaneZ);
+        }
+
+        protected override void UpdateViewMatrix()
+        {
+            // Make our view matrix
+            Matrix.CreateFromQuaternion(ref orientation, out matrix_View);
+
+            matrix_View.M41 = -Vector3.Dot(Right, position);
+            matrix_View.M42 = -Vector3.Dot(Up, position);
+            matrix_View.M43 = Vector3.Dot(Forward, position);
+            matrix_View.M44 = 1f;
+
+            // Create the combined view-projection matrix
+            Matrix.Multiply(ref matrix_View, ref matrix_Projection, out matrix_ViewProj);
+
+            // Update the bounding frustum
+            boundingFrustum.SetMatrix(matrix_ViewProj);
+        }
+
+        public override void Viewport(Viewport viewport)
+        {
+            // Create Projection Matrix
+            Perspective(viewport.AspectRatio, nearClipPlaneZ, farClipPlaneZ);
+
+            // Create the combined view-projection matrix
+            Matrix.Multiply(ref matrix_View, ref matrix_Projection, out matrix_ViewProj);
+
+            // Update the bounding frustum
+            boundingFrustum.SetMatrix(matrix_ViewProj);
+        }
+    }
+}
